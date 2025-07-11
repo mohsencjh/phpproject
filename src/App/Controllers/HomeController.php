b752903f221a73c180cd2a052268438e253d7ea2
mohsencjh <kiumars2365@Gmail.com>
2025-07-11 13:16:07 +0330

    1

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+
+use App\Config\Paths;
+
 class HomeController
 {
+
+  private TemplateEngine $view;
+
+  public function __construct()
+  {
+    $this->view = new TemplateEngine(Paths::VIEW);
+  }
+
   public function home()
   {
-    echo 'Home Page';
+    echo $this->view->render("/index.php", [
+      'title' => 'Home page'
+    ]);
   }
 }
---
+++ src/App/views/partials/_footer.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+<footer class="container mx-auto my-12 text-center">
+    &copy; PHPProject
+</footer>
+<!-- End Footer -->
+</body>
+
+</html>
